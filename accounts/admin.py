74e2fa8f7eb492f113d80242f39995fa08670c6f
ediberto <coronelvediberto@gmail.com>
2024-05-10 03:52:39 -0400

    agregando lista despegable

--- accounts/admin.py
+++ accounts/admin.py
@@ -9,7 +9,7 @@ from .models import Citas
 # Register your models here.
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id_paciente', 'dni','nombre', 'apellido', 'telefono', 'correo', 'fecha', 'hora' , 'mensaje', 'servicio', 'fecha_registro')
+    list_display = ('id_paciente', 'dni','nombre', 'apellido', 'telefono', 'correo', 'fecha', 'hora' , 'mensaje', 'fecha_registro')
     search_fields = ['nombre', 'apellidos', 'correo', 'telefono']
     list_filter = ['fecha_registro', 'sexo']
     list_per_page = 10
